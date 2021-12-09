0fab780bb6cb6f6734913f844f0122b52eea46c6
Joseph Lee <leethfc11@gmail.com>
2021-12-09 13:14:55 -0500

    implemented passport local and jwt strategies. now needs to fix the frontend.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
   async validateUser(username: string, password: string) {
     const user = await this.usersService.findUser(username);
-
     const match = await bcrypt.compare(password, user.password);
-
     if(match){
-      //login
+      return user;
     }
+    return null;
   }
 
+  async login(user: any) {
+    const payload = {username: user.username, sub: user.userId};
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get, Post, Body } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async addUser(@Body('username') username: string, @Body('password') password: string, @Body('name') name: string) {
+    const generatedId = await this.usersService.insertUser(username, password, name);
+    return {id: generatedId};
+  }
+
+  @Get(':username')
+  async getUser(@Param('username') username: string) {
+    const user = await this.usersService.findUser(username);
+    return user;
+  }
+}
