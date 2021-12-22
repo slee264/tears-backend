33ee935e36f31022f65e3a398e3c23b8f3a982b5
Joseph Lee <leethfc11@gmail.com>
2021-12-21 21:38:06 -0500

    configuring cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true});
+  const app = await NestFactory.create(AppModule, { cors: {
+    origin: process.env.DOMAIN,
+    credentials: true,
+  }});
   app.use(cookieParser(process.env.COOKIE_SECRET));
   await app.listen(5000);
 }
